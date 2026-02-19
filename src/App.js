1e8ea27cf0241ae2d269251eca26af677f5164cf
cremeni25 <andersonnavarro.si@gmail.com>
2026-02-19 13:01:39 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { supabase } from "./supabaseClient";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  return <Login />;
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getUser().then(({ data }) => {
+      setUser(data.user);
+    });
+
+    const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
+      setUser(session?.user || null);
+    });
+
+    return () => listener.subscription.unsubscribe();
+  }, []);
+
+  if (!user) {
+    return <Login />;
+  }
+
+  return <Dashboard user={user} />;
 }
 
 export default App;
